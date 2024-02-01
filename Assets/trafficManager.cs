b584e7fd07a0fda3fa1d445ab76e57c917cded55
Sebastian Hallabrin <sh363@student.london.ac.uk>
2024-02-01 17:42:32 +0100

    creating city part b

---
+++ Assets/trafficManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class TrafficManager : MonoBehaviour
+{
+    public GameObject carPrefab;
+    public Transform[] spawnPoints;
+    public float spawnFrequency = 2.0f;
+
+    private float lastSpawnTime = 0.0f;
+
+    void Update()
+    {
+        if (Time.time - lastSpawnTime > spawnFrequency)
+        {
+            SpawnCar();
+            lastSpawnTime = Time.time;
+        }
+    }
+
+    void SpawnCar()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(carPrefab, spawnPoints[spawnPointIndex].position, Quaternion.identity);
+    }
+}
