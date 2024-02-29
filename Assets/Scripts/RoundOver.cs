32548fc264ac2a9283ede0a53e6abc885da3b5dd
Pilar Huidobro <hello@pilar.codes>
2024-02-29 22:38:49 +0100

    Feat: Create script for when the play round is over

---
+++ Assets/Scripts/RoundOver.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoundOver : MonoBehaviour
+{
+    PauseMenu pauseMenuScript;
+    Timer timerObjectScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject timerObject = GameObject.Find("Timer");
+
+        timerObjectScript = timerObject.GetComponent<Timer> ();
+
+        if(!timerObjectScript.timerIsRunning) {
+            EndRound();
+        }
+
+    }
+
+
+    void EndRound() {
+		GameObject pauseMenuObject = GameObject.Find("Pause Menu");
+
+		pauseMenuScript = pauseMenuObject.GetComponent<PauseMenu> ();
+        pauseMenuScript.canPause = false;
+        Time.timeScale = 0;
+
+        GameObject roundOverUIObject = GameObject.Find("Round Over").transform.Find( "Round Over UI" ).gameObject;
+        roundOverUIObject.gameObject.SetActive(true);
+    }
+
+    public void PlayNextRound() {
+        SceneManager.LoadScene("allWorld");
+    }
+}
