4e03f741114ebb3c54b20ac45c9541f00d986ac1
Pilar Huidobro <hello@pilar.codes>
2024-03-07 18:45:23 +0100

    Feat: Persist window mode between sessions

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
+    [SerializeField] Toggle fullscreenToggle;
     bool runOnce = false;
 
     // Start is called before the first frame update
@@ -14,6 +15,18 @@ public class SettingsMenu : MonoBehaviour
         if(!PlayerPrefs.HasKey("volume"))
         {
             PlayerPrefs.SetFloat("volume", 1);
+        }
+
+        if(!PlayerPrefs.HasKey("fullscreen"))
+        {
+            PlayerPrefs.SetInt("fullscreen", 1);
+            Screen.fullScreen = true;
+        }
+        else
+        {
+            bool isFullScreen = Screen.fullScreenMode != FullScreenMode.Windowed;
+
+            Screen.fullScreen = isFullScreen;
         }
     }
 
@@ -26,6 +39,8 @@ public class SettingsMenu : MonoBehaviour
             AudioListener.volume = volume;
             volumeSlider.value = volume;
 
+            fullscreenToggle.isOn = Screen.fullScreenMode != FullScreenMode.Windowed;
+
             runOnce = true;
         }
     }
@@ -36,6 +51,22 @@ public class SettingsMenu : MonoBehaviour
         {
             AudioListener.volume = volumeSlider.value;
             PlayerPrefs.SetFloat("volume", volumeSlider.value);
+        }
+    }
+
+    public void ToggleFullscreen()
+    {
+        if(!runOnce) return;
+
+        Screen.fullScreen = !Screen.fullScreen;
+
+        if (Screen.fullScreenMode == FullScreenMode.Windowed)
+        {
+            PlayerPrefs.SetInt("fullscreen", 0);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("fullscreen", 1);
         }
     }
 }
