c3e12751dc233f158ed7bfb5ea2269afe5115b08
Barton-Loutit <94425592+Barton-Loutit@users.noreply.github.com>
2024-03-09 12:49:02 -0700

    Added Car Controller Value Flags
    Fence trigger to manage car speed in the park
    Fence trigger to manage car speed in the city
    Updated gravity
    Updated car COM

---
+++ Assets/Scripts/ParkEntrySpeedModifier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkEntrySpeedModifier : MonoBehaviour
+{
+    public carController carController;
+    [SerializeField] float parkMotorTorque = 4000f;
+    [SerializeField] float parkBrakeTorque = 3000f;
+    [SerializeField] float parkSteeringAngle = 30f;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            carController.setBrakeForce(parkBrakeTorque);
+            carController.setBrakeForce(parkBrakeTorque);
+            carController.setSteeringAngle(parkSteeringAngle);
+        }
+    }
+}
---
+++ Assets/Scripts/cityEntrySpeedModifier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cityEntrySpeedModifier : MonoBehaviour
+{
+    public carController carController;
+    [SerializeField] float cityMotorTorque = 9000f;
+    [SerializeField] float cityBrakeTorque = 2000f;
+    [SerializeField] float citySteeringAngle = 15f;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            carController.setBrakeForce(cityMotorTorque);
+            carController.setBrakeForce(cityBrakeTorque);
+            carController.setSteeringAngle(citySteeringAngle);
+        }
+    }
+}
